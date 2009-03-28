e3e55c6af7160285c7ab362e50c21d6ac91f1f99
Jared Camins-Esakov <jcamins@jcamins-laptop.(none)>
2009-03-28 13:24:55 -0400

    * Remove extraneous templates in main stylesheet * Sanitize index entries * Begin work on an intro

--- make.sh
+++ make.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-saxonb-xslt -s:full.xml -xsl:preprocess.xsl -o:processed.xml
+saxonb-xslt -s:full_fin.xml -xsl:preprocess.xsl -o:processed.xml
 #saxonb-xslt -s:full.xml -xsl:preprocess.xsl -o:processed.xml
 cat processed.xml | perl sanitize.pl > temp.xml
 saxonb-xslt -s:temp.xml -xsl:catalog.xsl -o:test.tex
 latex test.tex
-grep "^%NAMES:" test.tex | sed -e "s/%NAMES://" > names.idx
-grep "^%SUBJECT:" test.tex | sed -e "s/%SUBJECT://" > subject.idx
-grep "^%GEO:" test.tex | sed -e "s/%GEO://" > geo.idx
-grep "^%SERIES:" test.tex | sed -e "s/%SERIES://" > series.idx
+grep "^%NAMES:" test.tex | sed -e "s/%NAMES://" | sed -e "s/|/ /g" > names.idx
+grep "^%SUBJECT:" test.tex | sed -e "s/%SUBJECT://" | sed -e "s/|/ /g" > subject.idx
+grep "^%GEO:" test.tex | sed -e "s/%GEO://" | sed -e "s/|/ /g" > geo.idx
+grep "^%SERIES:" test.tex | sed -e "s/%SERIES://" | sed -e "s/|/ /g" > series.idx
 makeindex names
 makeindex subject
 makeindex geo
