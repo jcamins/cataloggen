ceb8283609d4832f82be4bc9ea93e6e0a2ec8a8c
Jared Camins-Esakov <jcamins@gmail.com>
2010-08-08 20:24:31 -0400

    Convert the build script to use xelatex
    XeLaTeX is Unicode-compliant, and therefore will enable the direct use
    of non-Roman scripts and eliminate the need for the sanitize script in
    its current form (instead the sanitize script will need to ensure that
    all MARC records are UTF-8 encoded).

--- make.sh
+++ make.sh
@@ -18,7 +18,7 @@ saxonb-xslt -s:full_fin.xml -xsl:preprocess.xsl -o:processed.xml
 #saxonb-xslt -s:full.xml -xsl:preprocess.xsl -o:processed.xml
 cat processed.xml | perl sanitize.pl > temp.xml
 saxonb-xslt -s:temp.xml -xsl:catalog.xsl -o:catalog.tex
-latex catalog.tex
+xelatex catalog.tex
 grep "^%NAMES:" catalog.tex | sed -e "s/%NAMES://" | sed -e "s/|/ /g" > names.idx
 grep "^%SUBJECT:" catalog.tex | sed -e "s/%SUBJECT://" | sed -e "s/|/ /g" > subject.idx
 grep "^%GEO:" catalog.tex | sed -e "s/%GEO://" | sed -e "s/|/ /g" > geo.idx
@@ -27,6 +27,6 @@ makeindex names
 makeindex subject
 makeindex geo
 makeindex -s series.ist series
-latex catalog.tex
-latex catalog.tex
+xelatex catalog.tex
+xelatex catalog.tex
 gs -q -dCompatibilityLevel=1.4 -dSubsetFonts=false -dPDFSETTINGS=/printer -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=catalog_final.pdf catalog.pdf -c '.setpdfwrite'
